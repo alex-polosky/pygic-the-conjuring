37fa305e59dabfbe37f6a7915b0a01bfc0a36b42
Alex Polosky <alexpolosky@gmail.com>
2025-07-06 21:53:39 -0400

    added tracking for tokenization

--- testing/test-consume.py
+++ testing/test-consume.py
@@ -25,10 +25,38 @@ pp_data = [
         for y in preprocess(test_data)
     for x in y
 ]
+got_words = []
+missing_words = {}
+
+def handle_bad_tokens(expr: str, ex: UnexpectedCharacters):
+    if ex.token_history:
+        prior_token = ex.token_history[-1]
+    else:
+        prior_token = None
+
+    try:
+        in_str = {s: s in expr[ex.pos_in_stream:] for s in (' .,:;')}
+        end_pos = min(expr.index(s, ex.pos_in_stream) for s in in_str if in_str[s]) if any(in_str.values()) else None
+    except ValueError:
+        end_pos = None
+
+    word = expr[ex.pos_in_stream:end_pos]
+    if word not in missing_words:
+        missing_words[word] = set()
+
+    missing_words[word].add(repr(prior_token))
+
+    if not end_pos:
+        return
+
+    next_expr = expr[end_pos + 1:]
+    try:
+        _ = parser.parse(next_expr)
+    except UnexpectedCharacters as ex:
+        handle_bad_tokens(next_expr, ex)
 
 good = 0
 total = 0
-missing_words = {}
 with open(os.path.join(BASE_DIR, 'test-consume.txt'), 'w', encoding='utf-8') as f:
     for data in pp_data:
         f.write(data + '\n')
@@ -38,26 +66,19 @@ with open(os.path.join(BASE_DIR, 'test-consume.txt'), 'w', encoding='utf-8') as 
             result = parser.parse(data)
             f.write(str(result) + '\n')
             good += 1
+            for token in result.children:
+                if token not in got_words:
+                    got_words.append(token)
         except UnexpectedCharacters as ex:
-            if ex.token_history:
-                prior_token = ex.token_history[-1]
-            else:
-                prior_token = None
-            try:
-                end_pos = data.index(' ', ex.pos_in_stream)
-            except ValueError:
-                end_pos = None
-            word = data[ex.pos_in_stream:end_pos]
-            if word not in missing_words:
-                missing_words[word] = set()
-            missing_words[word].add(repr(prior_token))
+            handle_bad_tokens(data, ex)
 
         f.write('\n')
 
 with open(os.path.join(BASE_DIR, 'test-consume.json'), 'w', encoding='utf-8') as f:
     json.dump({k: list(missing_words[k]) for k in sorted(missing_words.keys(), key=str.casefold)}, f, indent=4)
 
-print(f'Able to tokenize {good} / {total} : {((good / total) * 100):2.2f}%')
+print(f'Lines able to tokenize {good} / {total} : {((good / total) * 100):2.2f}%')
+print(f'Recognized tokens {len(got_words)} / {len(got_words) + len(missing_words)} : {(len(got_words) / (len(got_words) + len(missing_words)) * 100):2.2f}%')
 print('')
 
 pass
