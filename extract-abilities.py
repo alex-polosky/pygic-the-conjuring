9fc32ee3bb7d3dd61891ead89dd49849898ad68e
alex-polosky <alexpolosky@gmail.com>
2025-07-08 19:48:22 -0400

    added ability to pull mtg rules

--- extract-abilities.py
+++ extract-abilities.py
@@ -7,8 +7,10 @@ ab_search = r'701\.\d+\. (.*)'
 
 # TODO: implement checking existing keyword defs to warn about new defs that we've found?
 
+# TODO: implement ripping out abilities from magic rules
+
 if __name__ == '__main__':
-    with open(os.path.join(BASE_DIR, 'rules', 'abilities.txt'), encoding='utf-8') as f:
+    with open(os.path.join(BASE_DIR, 'data', 'abilities.txt'), encoding='utf-8') as f:
         text = f.read()
 
     with open(os.path.join(BASE_DIR, 'data', 'oracle_only.sorted.txt'), encoding='utf-8') as f:
@@ -46,7 +48,7 @@ if __name__ == '__main__':
             found_exts[ab] = set()
         found_exts[ab].add(ext)
 
-    with open(os.path.join(BASE_DIR, 'testing', 'abilities.lark'), 'w', encoding='utf-8') as f:
+    with open(os.path.join(BASE_DIR, 'out', 'abilities.lark'), 'w', encoding='utf-8') as f:
         f.write('%import primitives (_A,_B,_C,_D,_E,_F,_G,_H,_I,_J,_K,_L,_M,_N,_O,_P,_Q,_R,_S,_T,_U,_V,_W,_X,_Y,_Z)\n\n')
         f.write('ABILITY: ')
         first_line = True
---
+++ pull-magic-rules.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import os
+import requests
+import urllib.parse
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+URL = 'https://magic.wizards.com/en/rules'
+OUT = os.path.join(BASE_DIR, 'data')
+
+
+def get_html():
+    # TODO: error handling
+    response = requests.get(URL)
+    html = response.text
+    return html
+
+
+def make_soup(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.select('a.cta[href$=".txt"]')
+    if len(links) != 1:
+        raise IndexError
+    return links[0].attrs.get('href')
+
+
+def write_rules_from_link(link, encoding='utf-8'):
+    # encoding = response.encoding
+    # TODO: error handling
+    response = requests.get(link)
+    content = response.content.decode(encoding)
+    title = urllib.parse.unquote(link.split('/')[-1])
+    with open(os.path.join(OUT, title), 'w', encoding=encoding) as f:
+        f.write(content)
+
+
+def main():
+    html = get_html()
+    link = make_soup(html)
+    write_rules_from_link(link)
+
+if __name__ == '__main__':
+    main()
